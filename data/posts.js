1f9199aa106500a747434002688ac5cd2eb7b0f8
Ahmad Shahid <sn0259152@gmail.com>
2026-04-23 18:08:47 +0500

    Update posts.js

--- data/posts.js
+++ data/posts.js
@@ -6,7 +6,7 @@
 const posts = [
   {
     id: 100,
-    title: "Building Task Scalable Node.js Applications",
+    title: "Building AS Scalable Node.js Applications",
     category: "Web",
     author: "Ahmad Ansari",
     date: "March 10, 2026",
